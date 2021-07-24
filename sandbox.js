4f1ade48f265cee05d4f0d5b3f9c4b08d6b6080e
Michael Gallahan <gallahanm@gmail.com>
2021-07-23 21:52:19 -0400

    Initial pseudocode

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+const todoListElem = document.querySelector('#todo-list');
+let todoList = [];
+
+// createListItem(content)
+//   generate html li element with text and return
+
+// updateToDoList(todoList)
+// run createlistitem on each elem of the main array
+// set the innertext of ul to the generated html data
+
+// An event listener on add, filter, delete and edit
+// update the main array according to the change the user makes
+// send new array through updatetodolist()
