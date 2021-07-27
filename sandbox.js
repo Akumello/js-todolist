83b20d0e06f54bd32102aab8a4b52035140ad0af
Michael Gallahan <gallahanm@gmail.com>
2021-07-27 14:58:16 -0400

    Added function to create a list item to send back and be displayed

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,20 @@
 const todoListElem = document.querySelector('#todo-list');
+console.log(todoListElem);
 let todoList = [];
 
 // createListItem(content)
 //   generate html li element with text and return
+function createListItem(text, id)
+{
+    let li = document.createElement('li');
+    li.setAttribute('class', 'list-group-item d-flex justify-content-between');
+    li.innerHTML = `<div id="${id}">${text}</div><div><i class="fas fa-edit p-1"></i><i class="fas fa-trash-alt p-1"></i></div>`;
+    return li;
+}
+
+console.log(createListItem('Testing'));
+todoListElem.appendChild(createListItem('t', 1));
+console.log(todoListElem.innerHTML);
 
 // updateToDoList(todoList)
 // run createlistitem on each elem of the main array
@@ -11,3 +23,14 @@ let todoList = [];
 // An event listener on add, filter, delete and edit
 // update the main array according to the change the user makes
 // send new array through updatetodolist()
+
+
+// <li class="list-group-item d-flex justify-content-between">
+// <div>test</div>
+// <div>
+//   <i class="fas fa-edit"></i>
+//   <i class="fas fa-trash-alt"></i>
+// </div>
+// </li>
+// <li class="list-group-item">test</li>
+// <li class="list-group-item">test</li>
